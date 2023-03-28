56afe1f865adce2f0a0876b76dd4aeed963b990e
roby <robyjacob1998@gmail.com>
2023-03-29 01:48:38 +0530

    add empty notes page

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 className="header-title">Notes</h1>
+            <div className="header-btn" id="header-search"></div>
+            <div className="header-btn" id="header-info"></div>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+    return (
+        <main>
+            <img src="/src/assets/empty-note-logo.svg" className="empty-note-img"/>
+            <span className="empty-note-txt">Create your first note !</span>
+        </main>
+    )
+}
